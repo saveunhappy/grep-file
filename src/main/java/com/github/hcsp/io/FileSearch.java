1d957a3c640610cbf3a08b7ad044a9046a62f904
Mingru Li <limingru1991@gmail.com>
2019-08-19 17:56:56 +0800

    在文件中查找目标字符串行号 (#9)
    在文件中查找目标字符串行号 (#9)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,42 @@
 package com.github.hcsp.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        BufferedReader bufferedReader = null;
+        int rowNumber = 0;
+        try {
+            String line;
+            bufferedReader = new BufferedReader(new FileReader(target));
+            while ((line = bufferedReader.readLine()) != null) {
+                rowNumber++;
+                if (line.contains(text)) {
+                    return rowNumber;
+                }
+            }
+            return -1;
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(target.toString() + "不存在!", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(target.toString() + "无法读取!", e);
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
