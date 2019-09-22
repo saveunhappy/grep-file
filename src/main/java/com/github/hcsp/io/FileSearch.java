00fc8b7e5d1ee0c400510b7e5e7246d406e4d0dc
JinLingfeng <37367546+JinLingfeng@users.noreply.github.com>
2019-09-22 22:30:20 +0800

    异常处理 (#25)
    异常处理 (#25)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,42 @@
 package com.github.hcsp.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        FileReader fr = null;
+        BufferedReader bfr = null;
+        int lineNo = 0;
+        String line = "";
+        try {
+            fr = new FileReader(target);
+            bfr = new BufferedReader(fr);
+        } catch (FileNotFoundException e) {
+            //e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+
+        try{
+            while ((line = bfr.readLine())!=null){
+                lineNo++;
+                if (line.contains(text)){
+                    return lineNo;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+        return -1;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
