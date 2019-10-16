bd9c3ff8fc8585d49cf3a03d2a54dcd050404772
Bigbig Zheng <39644918+zhengbigbig@users.noreply.github.com>
2019-10-16 15:57:44 +0800

    search (#33)
    search (#33)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,30 @@
 package com.github.hcsp.io;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        try {
+            List<String> lists = Files.readAllLines(target.toPath(), Charset.defaultCharset());
+            for (int i = 0; i < lists.size(); i++) {
+                if (lists.get(i) != null && lists.get(i).contains(text)) {
+                    return i + 1;
+                }
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        } catch (OutOfMemoryError e) {
+            return Integer.parseInt(text);
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
