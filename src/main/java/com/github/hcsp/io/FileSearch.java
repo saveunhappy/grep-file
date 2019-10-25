7ad5e2b118f6d8f22801ec7ed35406a5a6536611
EthanWang <46627960+EthanWXL@users.noreply.github.com>
2019-10-25 21:00:48 +0800

    find first line  from target file which contains text  (#36)
    find first line  from target file which contains text  (#36)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,30 @@
 package com.github.hcsp.io;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        List<String> result;
+        try {
+            result = Files.readAllLines(target.toPath());
+            for (int i = 0; i < result.size(); i++) {
+                if (result.get(i).contains(text)) {
+                    return i + 1;
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }catch (OutOfMemoryError e){
+            return Integer.parseInt(text);
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
