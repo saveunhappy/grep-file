c708c8700a72188257f87a3f9ba6ef73dcb98c85
TangsongyuanmingQingHad <49867878+TangsongyuanmingQingHad@users.noreply.github.com>
2020-02-24 23:56:05 +0800

    FileUtils会报错 (#83)
    FileUtils会报错 (#83)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,15 +1,56 @@
 package com.github.hcsp.io;
 
+
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+//        List<String> list = null;
+//        int count = 1;
+//        try {
+//            list = FileUtils.readLines(target, Charset.defaultCharset());
+//            list = FileUtils.readLines(target,text);
+//            for (String message : list) {
+//                if (!message.equals(text)) {
+//                    count++;
+//                } else {
+//                    return count;
+//                }
+//            }
+//            return -1;
+//        } catch (IOException e) {
+//            throw new IllegalArgumentException(e);
+//        }
+
+        int i = 1;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(target));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains(text)) {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+
+    }
+
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println("结果行号：" + grep(new File(projectDir, "log.txt"), "BBB"));
     }
+
 }
