112696e934273b6a8d48d9e6503b6a0af93463f0
bonnie1115 <49628449+bonnie1115@users.noreply.github.com>
2020-02-27 16:26:05 +0800

    try catch匹配文件 (#85)
    try catch匹配文件 (#85)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,57 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
+
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+//用Files.readAllLines方法过不了单元测试的第三题，会抛outOfMemory?
+ /*       List<String> lines = null;
+        try {
+            lines = Files.readAllLines(Paths.get(String.valueOf(target)), StandardCharsets.UTF_8);
+            System.out.println("lines*****:" + lines);
+            int lineNum = 0;
+            for (String line : lines) {
+                lineNum++;
+                if (line.contains(text)) {
+                    return lineNum ;
+                }
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }*/
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(target));
+            int num = 0;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                num++;
+                if (line.contains(text)) {
+                    return num;
+                }
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
+         /*不需要关闭文件吗？
+         finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }*/
+
+    }
+
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
