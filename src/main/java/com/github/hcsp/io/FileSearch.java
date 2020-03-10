4cbf0e81f40eed001a6056d801a198695ee3edd8
zml59 <59412107+zml59@users.noreply.github.com>
2020-03-11 00:59:57 +0800

    查找第一个包含字符串的行数 (#90)
    查找第一个包含字符串的行数 (#90)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,32 @@
 package com.github.hcsp.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        int res = -1;
+        try {
+            BufferedReader lines = new BufferedReader(new FileReader(target));
+            String line = lines.readLine();
+            int linenumber = 1;
+            while (line != null) {
+                if (line.contains(text)) {
+                    res = linenumber;
+                    break;
+                }
+                linenumber++;
+                line = lines.readLine();
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("指定的文件不存在或者无法被读取");
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
