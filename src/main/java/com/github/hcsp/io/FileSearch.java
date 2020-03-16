0d698adafe017fa61efd28ceb896c90daf60a650
lolo <53633837+sun1341283@users.noreply.github.com>
2020-03-16 12:36:12 +0800

    完成文件的搜索指定行 (#95)
    完成文件的搜索指定行 (#95)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,29 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+
+    public static int grep(File target, String text) {
+
+        String line = null;
+        int lineNum = 1;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(target));
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(text)) {
+                    return lineNum;
+                }
+                lineNum++;
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("文件未找到或无法被读取");
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
