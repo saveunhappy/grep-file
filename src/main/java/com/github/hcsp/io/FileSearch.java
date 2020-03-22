e157db25316934a50da677bb6582a1ccfc24b8c1
举高高 <463927289@qq.com>
2020-03-22 23:46:03 +0800

    在文件中查找匹配的行号 (#102)
    在文件中查找匹配的行号 (#102)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,33 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        if (text == null || text.equals("")) {
+            throw new IllegalArgumentException("参数错误");
+        }
+        BufferedReader reader = null;
+        int temp = 0;
+        try {
+            reader = new BufferedReader(new FileReader(target.getAbsolutePath()));
+            String str;
+            while ((str = reader.readLine()) != null) {
+                temp++;
+                if (str.contains(text)) {
+                    return temp;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
