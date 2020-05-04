95effc9b49a087ec82166d80ab359eaee9ac6524
PudgeWL <nbhaohao@gmail.com>
2020-05-04 19:09:10 +0800

    feat: 完成挑战 (#117)
    feat: 完成挑战 (#117)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,35 @@
 package com.github.hcsp.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        int lineIncludesTextNumber = -1;
+        try {
+            int lineNumber = 1;
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(target));
+            while (true) {
+                String lineContent = bufferedReader.readLine();
+                if (lineContent == null) {
+                    break;
+                }
+                if (lineContent.contains(text)) {
+                    lineIncludesTextNumber = lineNumber;
+                    break;
+                }
+                lineNumber++;
+            }
+            return lineIncludesTextNumber;
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
