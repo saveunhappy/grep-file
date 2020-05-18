9853e6e5eb4e1eecce44b695036e62186825ccb7
YUCHEN LIU <181484908@QQ.COM>
2020-05-18 15:05:42 +0800

    work: grep-file (#120)
    work: grep-file (#120)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,30 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        int result = -1;
+        try (FileReader fileReader = new FileReader(target); BufferedReader bufferedReader = new BufferedReader(fileReader);) {
+            String line;
+            int index = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                index += 1;
+                if (line.contains(text)) {
+                    result = index;
+                    break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
