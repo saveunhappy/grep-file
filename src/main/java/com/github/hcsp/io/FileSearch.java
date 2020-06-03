c910a69c21cbc658a7b4d594c5c9662812e0289e
wong <63686228+laowang2020@users.noreply.github.com>
2020-06-03 09:52:20 +0800

    在文件中查找匹配的行号 (#129)
    在文件中查找匹配的行号 (#129)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,37 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        FileReader fr = null;
+        try {
+            fr = new FileReader(target);
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException();
+        }
+
+        BufferedReader br = new BufferedReader(fr);
+        int count = 0;
+        //循环读取文件内容
+        while (true) {
+            String s = "";
+            try {
+                if ((s = br.readLine()) == null) {
+                    return -1;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            count += 1;
+            if (s.contains(text)) {
+                return count;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
