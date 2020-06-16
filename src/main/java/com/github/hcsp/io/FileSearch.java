14fc4ddf4ff8aff89f342d46f804365f730c9313
McDul <907420318@qq.com>
2020-06-16 12:16:10 +0800

    重构了一下，之前的太长 (#134)
    重构了一下，之前的太长 (#134)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,35 @@
 package com.github.hcsp.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileSearch {
+    private static String line;
+
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(target));
+            return findLine(reader, text);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int findLine(BufferedReader reader, String text) throws IOException {
+        int count = 1;
+        while ((line = reader.readLine()) != null) {
+            if (line.contains(text)) {
+                return count;
+            }
+            count += 1;
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
