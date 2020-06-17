d3b597ccd5e0337ef0e72396602cfa0b80a5ef1d
DWB-Frank <57584830+DWB-Frank@users.noreply.github.com>
2020-06-18 01:24:05 +0900

    my commit (#135)
    my commit (#135)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,29 @@
 package com.github.hcsp.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        try {
+            BufferedReader bufferedReader = Files.newBufferedReader(target.toPath());
+            int i = 1;
+            for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine(), i++) {
+                if (line.contains(text)) {
+                    return i;
+                }
+            }
+
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
