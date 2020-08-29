911e0f4cb4731a712e4fdb0757ab83bcf19e42cc
Nat Chen <chen_natalie@outlook.com>
2020-08-29 21:00:32 +0800

    grep file (#146)
    grep file (#146)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,27 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        try (BufferedReader br = new BufferedReader(new FileReader(target))) {
+            String line;
+            int lineNumber = 1;
+            while ((line = br.readLine()) != null) {
+                if (line.contains(text)) {
+                    return lineNumber;
+                } else {
+                    lineNumber++;
+                }
+            }
+        } catch (IOException e) {
+           throw new IllegalArgumentException();
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
