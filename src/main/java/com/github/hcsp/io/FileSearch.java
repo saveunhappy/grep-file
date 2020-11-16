bd9b7f433a0d3484f3ad75aaa7269f52c9c3166c
sgz886 <sgz886@gmail.com>
2020-11-16 22:42:43 +0800

    BufferedReader 和 IllegalArgumentException (#153)
    BufferedReader 和 IllegalArgumentException (#153)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,32 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        if (!target.exists() || target.isDirectory() || !target.canRead()) {
+            throw new IllegalArgumentException("文件不存在或者无法read");
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(target))) {
+            String line;
+            int row_number = 1;
+            while ((line = br.readLine()) != null) {
+                if (line.contains(text)) {
+                    return row_number;
+                } else {
+                    row_number++;
+                }
+            }
+            return -1;
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("文件不存在");
+        } catch (IOException e) {
+            throw new IllegalArgumentException("OI错误");
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
