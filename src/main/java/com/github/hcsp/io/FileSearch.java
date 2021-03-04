86fc27f69109329127ca2b60f1b660a202c49d5a
Lian Yongbo <L1anyX@126.com>
2021-03-04 16:48:21 +0800

    IO与异常实战：在文件中查找匹配的行号 (#160)
    IO与异常实战：在文件中查找匹配的行号 (#160)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,25 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(target));
+            String line;
+            for (int i = 1; (line = br.readLine()) != null; i++) {
+                if (line.contains(text)) {
+                    return i;
+                }
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("文件不存在或者无法被读取！", e);
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
