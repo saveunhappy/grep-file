16d7f423e56f7a99490d460c3d9cfb470320c04f
jianqibot <85292386+jianqibot@users.noreply.github.com>
2021-08-10 00:02:25 +1000

    job done (#167)
    job done (#167)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,30 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        String line;
+        int lineNumber = 0;
+        try (LineIterator it = FileUtils.lineIterator(target, "UTF-8")) {
+            while (it.hasNext()) {
+                lineNumber++;
+                line = it.nextLine();
+                if (line.replaceFirst("^0+(?!$)", "").equals(text)) {
+                    return lineNumber;
+                }
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("file does not exist or reading error");
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
