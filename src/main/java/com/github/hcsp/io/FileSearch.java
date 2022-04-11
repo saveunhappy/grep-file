a3176ca5b23858442da233a77acdfd629af27d44
magicalmuggle <richardhtw@outlook.com>
2022-04-11 18:44:30 +0800

    在文件中查找匹配的行号 (#186)
    在文件中查找匹配的行号 (#186)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,31 @@
 package com.github.hcsp.io;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        try {
+            LineIterator lineIterator = FileUtils.lineIterator(target, StandardCharsets.UTF_8.toString());
+            int lineNumber = 1;
+            while (lineIterator.hasNext()) {
+                if (lineIterator.nextLine().contains(text)) {
+                    return lineNumber;
+                }
+                lineNumber++;
+            }
+            return -1;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
