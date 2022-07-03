fa94360d113be954864bc3c5231ef9abdb7e499c
yangyangsimons <97254476+yangyangsimons@users.noreply.github.com>
2022-07-03 20:44:46 +0100

    use bufferedRead to handle the huge file (#192)
    use bufferedRead to handle the huge file (#192)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,37 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.List;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        int result = -1;
+        int index = 1;
+        List<String> lines = null;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(target));
+
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
+                if (line.contains(text)) {
+                    result = index;
+                    break;
+                }
+                index++;
+            }
+
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
