56c29bfebbeaf46b64d8a1b1a0cc47457dca9a12
ang tsang <tsang1205@qq.com>
2022-07-03 17:52:39 +0800

    feat: grep file (#191)
    feat: grep file (#191)

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,31 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        int result = 1;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(target));
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    return -1;
+                }
+                if (line.contains(text)) {
+                    break;
+                }
+                result += 1;
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
